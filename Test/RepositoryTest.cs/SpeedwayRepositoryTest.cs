5befc9f98bd888bf1529f1c3303b7a35cd28bf40
LTI <LTI@INFEDL31041.lntinfotech.com>
2021-10-14 15:52:49 -0500

    fixing heroku container login automation

--- Test/RepositoryTest.cs/SpeedwayRepositoryTest.cs
+++ Test/RepositoryTest.cs/SpeedwayRepositoryTest.cs
@@ -55,7 +55,6 @@ namespace test.Repositories
             Driver driver = new Driver(testDriverDto);
             await _repo.AddDriverAsync(driver);
             List<Driver> Participants = await _db.Drivers.ToListAsync();
-            //RaceDto TestRaceDto = new RaceDto() { Name = "Tow", Category = CategoryEnum.Drag, Date = raceDate, Winner = winner, ParticipantsIds = Participants };
             Race testRace =new Race() { Name = "Tow", Category = CategoryEnum.Drag, Date = raceDate, Winner = winner };
             await _repo.AddRaceAsync(testRace);
             await _repo.SaveAsync();
@@ -95,20 +94,17 @@ namespace test.Repositories
             var winner = new Guid();
             var raceDate = new DateTime(1999, 1, 1);
             List<Guid> ParticipantIds = await _db.Drivers.Select(driver => driver.Id).ToListAsync();
-            //RaceDto TestRaceDto = new RaceDto() { Name = "Tow", Category = CategoryEnum.Drag, Date = raceDate, Winner = winner, ParticipantsIds = ParticipantIds };
            Race race = new Race(){ Name = "Tow", Category = CategoryEnum.Drag, Date = raceDate, Winner = winner};
             await _repo.AddRaceAsync(race);
             var winner2 = new Guid();
             var raceDate2 = new DateTime(1999, 1, 1);
             List<Guid> ParticipantIds2 = await _db.Drivers.Select(driver => driver.Id).ToListAsync();
-          // RaceDto TestRaceDto2 = new RaceDto() { Name = "Tow", Category = CategoryEnum.Drag, Date = raceDate2, Winner = winner2, ParticipantsIds = ParticipantIds2 };
             Race race2 = new Race(){ Name = "Tow", Category = CategoryEnum.Drag, Date = raceDate2, Winner = winner2 };
             await _repo.AddRaceAsync(race2);
             await _repo.SaveAsync();
             var todos = await _repo.GetAllRacesAsync();
             todos.Should().HaveCount(2);
         }
-
 
     }
 }
